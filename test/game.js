4e026c9dd2eadbf7cc779d669e0f19c5e25a447b
Bibix93 <baraa.elalami@gmail.com>
2021-11-18 10:33:01 +0000

    finished tests

--- test/game.js
+++ test/game.js
@@ -191,75 +191,28 @@ contract('Games', (accounts) => {
 
   });
 
+  it('should not let non admin to change the game status', async () => {
+    const gameInstance = await Games.deployed();
+    try { 
+      await gameInstance.changeGameStatus(1, {from: accounts[1]});
+    } catch(e) {
+      return e;
+    }
+    const game = await gameInstance.game.call();
 
+    assert.equal(game[2], 0, "Game status shouldn't change");
+  });
 
+  it('should change the game status and update balances of accounts[0] and account[1]', async () => {
+    const gameInstance = await Games.deployed();
+    await gameInstance.changeGameStatus(1);
+    const game = await gameInstance.game.call();
+    const balanceZero = await gameInstance.getBalance(accounts[0]);
+    const balanceOne = await gameInstance.getBalance(accounts[1]);
 
+    assert.equal(game[2], 1, "Game status should change");
+    assert.equal(balanceZero, web3.utils.toWei('4118', "finney"), "Wrong balance");
+    assert.equal(balanceOne, web3.utils.toWei('882', "finney"), "Wrong balance");
+  });
 
-
-
-//     it('match bet ', async () => {
-//         const gameInstance = await Game.deployed();
-//         await gameInstance.placeBet(1, 1, 300, {from : accounts[1], value: 2*1e+15 });
-//         const betCount = await gameInstance.betCount.call();
-//         const status1 = await gameInstance.getBetStatus(1);
-//         const status2 = await gameInstance.getBetStatus(2);
-
-//         assert.equal(status1, 1, "Bet unmatched");
-//         assert.equal(status2, 1, "Bet unmatched");
-//         assert.equal(betCount, 2, "Bet unplaced");
-// });
-
-//     it('should change game status', async () => {
-//         const gameInstance = await Games.deployed();
-//         await gameInstance.changeGameStatus(1, {from : accounts[0]});
-//         const winner = await gameInstance.game.call();
-
-//         assert.equal(winner[3], 1, "Game status not changed");
-//   });
-
-
-  
-
-  //   it('should not let change game status', async () => {
-//     const gamesInstance = await Games.deployed();
-//     await gamesInstance.changeGameStatus(1, {from : accounts[1]});
-//     const winner = await gamesInstance.game.call();
-
-//     assert.equal(winner[3], 0, "Only admin can change game status");
-//   });
-
-
-
-
-
-//   it('should call a function that depends on a linked library', async () => {
-//     const metaCoinInstance = await MetaCoin.deployed();
-//     const metaCoinBalance = (await metaCoinInstance.getBalance.call(accounts[0])).toNumber();
-//     const metaCoinEthBalance = (await metaCoinInstance.getBalanceInEth.call(accounts[0])).toNumber();
-
-//     assert.equal(metaCoinEthBalance, 2 * metaCoinBalance, 'Library function returned unexpected function, linkage may be broken');
-//   });
-//   it('should send coin correctly', async () => {
-//     const metaCoinInstance = await MetaCoin.deployed();
-
-//     // Setup 2 accounts.
-//     const accountOne = accounts[0];
-//     const accountTwo = accounts[1];
-
-//     // Get initial balances of first and second account.
-//     const accountOneStartingBalance = (await metaCoinInstance.getBalance.call(accountOne)).toNumber();
-//     const accountTwoStartingBalance = (await metaCoinInstance.getBalance.call(accountTwo)).toNumber();
-
-//     // Make transaction from first account to second.
-//     const amount = 10;
-//     await metaCoinInstance.sendCoin(accountTwo, amount, { from: accountOne });
-
-//     // Get balances of first and second account after the transactions.
-//     const accountOneEndingBalance = (await metaCoinInstance.getBalance.call(accountOne)).toNumber();
-//     const accountTwoEndingBalance = (await metaCoinInstance.getBalance.call(accountTwo)).toNumber();
-
-
-//     assert.equal(accountOneEndingBalance, accountOneStartingBalance - amount, "Amount wasn't correctly taken from the sender");
-//     assert.equal(accountTwoEndingBalance, accountTwoStartingBalance + amount, "Amount wasn't correctly sent to the receiver");
-//   });
 });
